701c850327faa45dc503e21a6f8d78ac09b81838
Shivam Gera <Shivamgera@github.com>
2020-01-14 16:24:40 +0530

    add views

--- elastic/views.py
+++ elastic/views.py
@@ -1,11 +1,57 @@
 from rest_framework.generics import ListAPIView
 
+from django_elasticsearch_dsl_drf.constants import (
+    SUGGESTER_TERM,
+    SUGGESTER_PHRASE,
+    SUGGESTER_COMPLETION,
+)
 from .models import Movies
 from .documents import MoviesDocument
 from .serializers import MoviesDocumentSerializer
+from rest_framework.pagination import CursorPagination
+from django_elasticsearch_dsl_drf.filter_backends import (
+    FilteringFilterBackend,
+    DefaultOrderingFilterBackend,
+    OrderingFilterBackend,
+    SearchFilterBackend,
+    SuggesterFilterBackend,
+    FunctionalSuggesterFilterBackend,
+)
+from django_elasticsearch_dsl_drf.viewsets import DocumentViewSet
 
 
-class MoviesView(ListAPIView):
+class MoviesDocumentViewSet(DocumentViewSet):
     document = MoviesDocument
     serializer_class = MoviesDocumentSerializer
-    lookup_field =
+    lookup_field = 'id'
+    filter_backends= [
+        FilteringFilterBackend,
+        DefaultOrderingFilterBackend,
+        OrderingFilterBackend,
+        SearchFilterBackend,
+        SuggesterFilterBackend
+    ]
+    pagination_class = CursorPagination
+    search_fields=(
+        'title',
+        'genre'
+    )
+    filter_fields = {
+        'genre'
+    }
+    ordering_fields = {
+        'title':'title.raw',
+        'id':None,
+    }
+    ordering = 'title.raw'
+    suggester_fields = {
+        'name_suggest':{
+            'field':'title.suggest',
+            'suggesters':[
+                SUGGESTER_TERM,
+                SUGGESTER_PHRASE,
+                SUGGESTER_COMPLETION,
+            ]
+        }
+    }
+
