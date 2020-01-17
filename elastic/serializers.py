2bd9a81367dac2aaeceb50683456726e557c6301
Shivam Gera <Shivamgera@github.com>
2020-01-17 20:34:36 +0530

    add file

--- elastic/serializers.py
+++ elastic/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 
 
-class MoviesElasticSerializer(serializers.Serializer):
+class MoviesDocumentSerializer(serializers.Serializer):
     title = serializers.CharField()
     year = serializers.IntegerField()
     rating = serializers.FloatField()
     genre = serializers.CharField()
+
+class MoviesSuggestionsSerializer(serializers.Serializer):
+    title = serializers.CharField()
+    score = serializers.IntegerField()
