c20a858ea073e65fa99937b5e0f0c6854227e1ef
Shivam Gera <gera.shivam@gmail.com>
2020-01-17 20:42:32 +0530

    Update

--- elastic/urls.py
+++ elastic/urls.py
@@ -4,7 +4,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from elastic.views import MoviesView, MoviesAutoCompleteView, SuggestionsView
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    #url(r'^', include(router.urls)),
     path('search/', MoviesView.as_view(), name='movies'),
     path('auto_complete/', MoviesAutoCompleteView.as_view(), name='suggest'),
     url('suggest/', SuggestionsView.as_view(), name='suggestions'),
